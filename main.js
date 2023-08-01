a37164bb57f5087839e9a8c8721230a5efa746c7
Ivan Saldana <ivansaldana@Ivans-MacBook-Pro.local>
2023-08-01 11:40:50 +0200

    cors omit option

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 import './style.css';
 
-fetch('https://firstapi-seven.vercel.app/todos',{
-  method:'get'
+fetch('https://firstapi-seven.vercel.app/todos', {
+  method: 'get',
+  cors:"omit"
 })
-.then(resp=>resp.json())
-.then(data=>{
-  console.log(data);
-  let ul=document.getElementById('ul_todo');
-  data.forEach(todo => {
-       ul.appendChild(createLi(todo.title));
+  .then(resp => resp.json())
+  .then(data => {
+    console.log(data);
+    let ul = document.getElementById('ul_todo');
+    data.forEach(todo => {
+      ul.appendChild(createLi(todo.title));
+    });
   });
-});
 
-const createLi=(content)=>{
-  let li=document.createElement('li');
-  li.innerHTML=content;
+const createLi = (content) => {
+  let li = document.createElement('li');
+  li.innerHTML = content;
   return li;
 }
 
